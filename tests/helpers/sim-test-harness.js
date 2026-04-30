be2e6489d93a89df281080995d8d5a2169b40cb6
Zimon <zimon.roos@icloud.com>
2026-04-30 08:15:10 +0200

    Fix script path for simulation constants in test harness

--- tests/helpers/sim-test-harness.js
+++ tests/helpers/sim-test-harness.js
@@ -90,7 +90,7 @@ function createHarness(options = {}) {
 
   const vmContext = vm.createContext(context);
 
-  loadScript(vmContext, "public/simulation-constants.js");
+  loadScript(vmContext, "simulation-constants.js");
 
   vmContext.__simOverrides = simOverrides;
   vm.runInContext(
